b4d432372b1bda1f06779a2de134d44b6ba4c5c3
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-05 18:34:54 +0200

    details Service Component

--- src/components/ServiceComponent.vue
+++ src/components/ServiceComponent.vue
@@ -1,46 +1,61 @@
 <template>
-  <div class="box-service">
+  <div class="box-service px-4 container-fluid">
+    <div class="d-flex box-card mx-5 px-5 row justify-content-center">
+      <div class="d-flex card-service mx-5 px-5">
 
-    <div class="d-flex box-card">
 
-      <div class="card bg-transparent text-center">
-        <i class="text-danger fa-solid fa-link"></i>
-        <h5>Strength & Conditioning</h5>
+        <!---CARD 1-->
+        <div class="d-flex align-items-center  card bg-transparent text-center">
+          <i id="border" class="fs-4 text-danger fa-solid fa-link"></i> <!--ADD ICON--->
+          <h5>Strength & Conditioning</h5>
 
+          <img src="@/assets/images/divider-x-red.png" alt="">
+
+        
+
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+        </div>
+
+        <!---CARD 2-->
+        <div class="card align-items-center bg-transparent text-center">
+          <i id="border" class="fs-4 text-danger fa-solid fa-bicycle"></i><!--ADD ICON--->
+          <h5>Fitness & Cardio</h5>
+
+          <img src="@/assets/images/divider-x-red.png" alt="">
+
+        
+
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+        </div>
+        <!---CARD 3-->
+        <div class="card align-items-center bg-transparent text-center">
+          <i id="border" class="fs-4 text-danger fa-solid fa-cloud"></i><!--ADD ICON--->
+          <h5>Flexibility & Rest</h5>
+
+          <img src="@/assets/images/divider-x-red.png" alt="">
+
+        
+
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+        </div>
+        <!---CARD 4-->
+      <div class="card  align-items-center bg-transparent text-center">
+        <i id="border" class="fs-4 text-danger fa-solid fa-heart-pulse"></i><!--ADD ICON--->
+        <h5>Health & Diet</h5>
+
+        <img src="@/assets/images/divider-x-red.png" alt="">
         
 
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
 
       </div>
-      <div class="card bg-transparent text-center">
-        <i class="text-danger fa-solid fa-bicycle"></i>
-        <h5>Strength & Conditioning</h5>
 
-        
-
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
 
       </div>
-
-      <div class="card bg-transparent text-center">
-        <i class="text-danger fa-solid fa-cloud"></i>
-        <h5>Strength & Conditioning</h5>
-
-        
-
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
-
-      </div>
-
-      <div class="card  bg-transparent text-center">
-        <i class="text-danger fa-solid fa-heart-pulse"></i>
-        <h5>Strength & Conditioning</h5>
-
-        
-
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
-
-      </div>
+      
 
 
     </div>
@@ -55,10 +70,11 @@ name: 'ServiceComponent'
 }
 </script>
 
-<style lang="scss" scooped>
+<style lang="scss" scoped>
 .box-service {
   background-image: url(@/assets/images/gym_bkgd_bw-compressor.jpg);
   background-size: contain;
+  max-width: 100%;
 }
 .card {
   color:white;
@@ -68,5 +84,21 @@ name: 'ServiceComponent'
   color: none;
 }
 
+p {
+  font-size: 12px;
+  color: #6b6b6b;
+}
+
+img {
+  height: 10px;
+  width: 10px;
+ 
+}
+
+#border {
+  border-radius: 100%;
+  border: 2px solid red;
+  padding: 20px;
+}
 
 </style>
