7df9c4cb9fd5e0d6fac88f1d4dd33fbc8ecd9293
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-05 17:24:51 +0200

    add icon Service Component

--- src/components/ServiceComponent.vue
+++ src/components/ServiceComponent.vue
@@ -1,5 +1,52 @@
 <template>
-  <div></div>
+  <div class="box-service">
+
+    <div class="d-flex box-card">
+
+      <div class="card bg-transparent text-center">
+        <i class="text-danger fa-solid fa-link"></i>
+        <h5>Strength & Conditioning</h5>
+
+        
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+      </div>
+      <div class="card bg-transparent text-center">
+        <i class="text-danger fa-solid fa-bicycle"></i>
+        <h5>Strength & Conditioning</h5>
+
+        
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+      </div>
+
+      <div class="card bg-transparent text-center">
+        <i class="text-danger fa-solid fa-cloud"></i>
+        <h5>Strength & Conditioning</h5>
+
+        
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+      </div>
+
+      <div class="card  bg-transparent text-center">
+        <i class="text-danger fa-solid fa-heart-pulse"></i>
+        <h5>Strength & Conditioning</h5>
+
+        
+
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, rem!</p>
+
+      </div>
+
+
+    </div>
+
+
+  </div>
 </template>
 
 <script>
@@ -8,6 +55,18 @@ name: 'ServiceComponent'
 }
 </script>
 
-<style>
+<style lang="scss" scooped>
+.box-service {
+  background-image: url(@/assets/images/gym_bkgd_bw-compressor.jpg);
+  background-size: contain;
+}
+.card {
+  color:white;
+  margin: 70px;
+  gap:20px;
+  padding: 15px;
+  color: none;
+}
+
 
 </style>
