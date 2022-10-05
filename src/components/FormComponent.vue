f7415f192c15f7551ebb145053830490d04d0ff6
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-05 16:40:15 +0200

    details form component

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,43 +1,54 @@
 <template>
+<div>
   <div class="box2">
-    <div class="row">
-      <div class="col-12">
 
-        <div class="text-center py-5 box-fitness my-3">
-          <h2>WORLD CLASS Fitness</h2>
-          <img src="@/assets/images/divider-xx-red.png" alt="">
-          <div class="mx-5 py-5 text-center">
+    <div class="text-center fitness py-5">
+      <h2>WORLD CLASS Fitness</h2>
+        <img src="@/assets/images/divider-xx-red.png" alt="img">
+        <div class="text-center col-12">
             <p class="mx-5">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we're equipped to hel you reach your goals of a healthy and fit lifestyles</p>
 
-          </div>
-      
-
-        
         </div>
 
-        <div class="box-form">
-          <div class="form">
+          
+     
+    </div>
 
+    <div class="box-form">
+        <div class="container-fluid text-center py-5">
 
+          <div class="row py-5">
+
+            <div class="col-6 text-end">
+              <img src="@/assets/images/home_img-compressor-400x222.jpg" alt="">
+
+            </div>
+
+            <div class="col-6 text-start">
+
+              <h5 class="pb-2">Sign Up For a Free Training Session!</h5>
+              <p>Name:</p>
+              <input class="mb-3" type="text" placeholder="name">
+              <p>Email:</p>
+              <input type="text" placeholder="Email">
+              <div class="py-4">
+                <button class="btn-start">START NOW</button>
+              </div>
+              
+
+            </div>
           </div>
-
-
-
-
         </div>
-      
-
-
-      </div>
 
 
 
 
     </div>
-    
 
 
   </div>
+  
+</div>
  
 </template>
 
@@ -49,12 +60,27 @@ name: 'FormComponent'
 
 <style lang="scss" scoped>
 .box2 {
-
   margin-top: 150px;
   color: white;
+  padding-top: 10px;
 }
 
-.box-fitness {
+.fitness {
   background-color: #2e2c2c;
+}
+
+.box-form {
+  background-color: hsla(0, 0%, 12%, 1);
+}
+
+input {
+  background-color: #2e2c2c;
+  width: 330px;
+}
+
+.btn-start {
+  width: 330px;
+  background-color: #d5625f;
+  color:white;
 }
 </style>
