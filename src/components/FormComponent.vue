7899699743010f41ddacdfd946604252ddafae46
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-05 15:52:44 +0200

    create box-fitness

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,5 +1,44 @@
 <template>
-  <div></div>
+  <div class="box2">
+    <div class="row">
+      <div class="col-12">
+
+        <div class="text-center py-5 box-fitness my-3">
+          <h2>WORLD CLASS Fitness</h2>
+          <img src="@/assets/images/divider-xx-red.png" alt="">
+          <div class="mx-5 py-5 text-center">
+            <p class="mx-5">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we're equipped to hel you reach your goals of a healthy and fit lifestyles</p>
+
+          </div>
+      
+
+        
+        </div>
+
+        <div class="box-form">
+          <div class="form">
+
+
+          </div>
+
+
+
+
+        </div>
+      
+
+
+      </div>
+
+
+
+
+    </div>
+    
+
+
+  </div>
+ 
 </template>
 
 <script>
@@ -8,6 +47,14 @@ name: 'FormComponent'
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.box2 {
 
+  margin-top: 150px;
+  color: white;
+}
+
+.box-fitness {
+  background-color: #2e2c2c;
+}
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,33 +1,42 @@
 <template>
     <div class="box" >
-
+        
         <div class="d-flex justify-content-between container" >
 
-        <div class="logo-header" >
-            <img src="@/assets/images/gym_logo_1x.png" alt="logo">
-        </div> 
-        <div class="d-flex gap-4 navbar-left" >
-            <div v-for="link in navbartext" :key="link.text" class="navbar" >
-            <a href="">{{ link.text }}</a>
+            <div class="logo-header" >
+                <img src="@/assets/images/gym_logo_1x.png" alt="logo">
+            </div> 
+            <div class="d-flex gap-4 navbar-left" >
+                <div v-for="link in navbartext" :key="link.text" class="navbar" >
+                <a href="">{{ link.text }}</a>
 
 
-        </div>
-
-        </div>
-        
-        </div>
-
-        <div class="jumbotron" >
-
-            <img src="@/assets/images/home-banner.jpg" alt="home-banner">
-
-            <div class="text-center title" >
-                <h1>NO PAIN NO GAIN</h1>
-                <p>Motivation is what gets you started. Habit is what keeps you going.</p>
-
-                <button class="btn" >JOIN US TODAY</button>
+                </div>
 
             </div>
+        
+        </div>
+        <div>
+
+
+            
+        </div>
+        <div class="jumbotron" >
+            <div class="box2" >
+                <img src="@/assets/images/home-banner.jpg" alt="home-banner">
+
+                    <div class="text-center title my-5" >
+                    <h1>NO PAIN NO GAIN</h1>
+                    <p>Motivation is what gets you started. Habit is what keeps you going.</p>
+
+                    <button class="btn" >JOIN US TODAY</button>
+
+                </div>
+
+
+            </div>
+
+            
 
         </div>
     </div>
@@ -50,7 +59,7 @@ props : {
     width: 1200px;
     margin: 0 auto;
     padding: 20px 0px;
-
+    
 }
 
 .logo-header img {
@@ -71,16 +80,15 @@ a {
 
 .jumbotron img {
     width: 100%;
-    background-size: cover;
     position: absolute;
     z-index: -1;
     top: 0;
+    background-size: cover;
 }
-
 .title {
-    position: relative;
-    top: 200px;
     color:white ;
+    position: relative;
+    top: 90px;
 }
 h1 {
     font-size:80px
