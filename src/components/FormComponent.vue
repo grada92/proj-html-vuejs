257363c62bd5ec6420e8d227e5c14cab469e9bf9
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-06 19:57:45 +0200

    add details Form Component

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
-  <div class="box2">
+  <div class="box2 pt-3">
 
     <div class="text-center fitness py-5">
-      <h2>WORLD CLASS Fitness</h2>
+      <h2 class="py-3" >WORLD CLASS Fitness</h2>
         <img src="@/assets/images/divider-xx-red.png" alt="img">
         <div class="text-center col-12">
-            <p class="mx-5">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we're equipped to hel you reach your goals of a healthy and fit lifestyles</p>
+            <p class="mx-5 py-4 second-text">At Avada Gym, everything we do from top to bottom is of the highest quality to ensure we're equipped to hel you reach your goals of a healthy and fit lifestyles</p>
 
         </div>
 
@@ -14,25 +14,27 @@
      
     </div>
     
-    <div class="box-form py-5">
-        <div class="container-fluid text-center py-5">
+    <div class="box-form pb-5">
+        <div class="container-fluid text-center pb-5">
           
-          <div class="row py-5">
-            
-            <div class="col-6 text-end">
+          <div class="row pb-5 text-center">
+            <div class="col-12">
+              <img class="triangle" src="@/assets/images/img-triangle.png" alt="triangle">
+            </div>
+            <div class="col-6 text-end pb-5">
               
-              <img src="@/assets/images/home_img-compressor-400x222.jpg" alt="">
+              <img class="img-form" src="@/assets/images/home_img-compressor-400x222.jpg" alt="">
 
             </div>
 
-            <div class="col-6 text-start">
+            <div class="col-6 text-start pb-5">
 
-              <h5 class="pb-2">Sign Up For a Free Training Session!</h5>
+              <h5 class="pb-2 fs-4">Sign Up For a Free Training Session!</h5>
               <p>Name:</p>
               <input class="mb-3" type="text" placeholder="name">
               <p>Email:</p>
               <input type="text" placeholder="Email">
-              <div class="py-4">
+              <div class="py-4 pt-5">
                 <button class="btn-start">START NOW</button>
               </div>
               
@@ -65,12 +67,13 @@ name: 'FormComponent'
   color: white;
   padding-top: 10px;
 }
-.img-1 {
+.triangle {
   fill: #2e2c2c;
-  margin: -200px 0px 0px 0px;
+  margin: -120px -25px 0px 0px;
 }
 .fitness {
   background-color: #2e2c2c;
+  height: 300px;
 }
 
 .box-form {
@@ -79,12 +82,19 @@ name: 'FormComponent'
 
 input {
   background-color: #2e2c2c;
-  width: 330px;
+  width: 430px;
 }
 
+.img-form {
+  width:500px
+}
 .btn-start {
-  width: 330px;
+  padding: 10px;
+  width: 430px;
   background-color: #d5625f;
   color:white;
+}
+.second-text {
+  color: #6b6b6b;
 }
 </style>
