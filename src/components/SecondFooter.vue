a4ca8d06fd66fc5450b1537cae43d5b46e6bef75
grada92 <107757307+grada92@users.noreply.github.com>
2022-10-08 10:20:17 +0200

    add social icon and button in SecondFooter

--- src/components/SecondFooter.vue
+++ src/components/SecondFooter.vue
@@ -1,6 +1,7 @@
 <template>
-<div class="box-footer py-5">
-    <div class="container-md pt-5">
+<div class="second-footer pb-2">
+    <div class="box-footer">
+        <div class="container-md py-5">
         <div class="row">
             <div class="col-3">
                 <h6 class="py-3">COME FIND US</h6>
@@ -38,7 +39,50 @@
         </div>
     </div>
 
+
+    </div>
+    
+    <div class="box-second-footer container-md py-4">
+        <div class="row">
+            <div class="col-8">
+                <a class="pe-4" href="#">Copyright 2012-2020</a> <span>|</span>
+                <a class="pe-4" href="#">Avada Theme by Theme Fusion</a><span>|</span>
+                <a class="pe-4" href="#">All Rights Reserved</a><span>|</span>
+                <a class="pe-4" href="#">Powered by Wordpress</a>
+            </div>
+
+            <div class="col-4">
+                <div class="social-footer d-flex gap-2 px-5">
+                    <!---Social Icon-->
+                    <div class="fb text-white">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    </div>
+
+                    <div class="tw text-white">
+                    <i class="fa-brands fa-twitter"></i>
+                    </div>
+
+                    <div class="ig text-white">
+                    <i class="fa-brands fa-instagram"></i>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        
+
+
+        </div>
+        <div class="container-fluid">
+            <div class="row me-5">
+            <div class="col-12 text-end me-5">
+                <button> <i class="fa-solid fa-angle-up"></i></button>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 </template>
 
 <script>
@@ -56,5 +100,43 @@ name: 'SecondFooter'
     color:#7a7a7a;
 } hr {
     color: #7a7a7a;
+ 
 }
+.second-footer {
+    background-color: #212425;
+}
+a {
+    text-decoration: none;
+    color:#aeafae;
+    font-size: 13px;
+}
+span {
+    color:#aeafae;
+    padding-right: 20px;
+}
+.social-footer {
+    margin-left: 210px;
+}
+.fb {
+  background-color:  #d3615e;
+  padding: 5px 12px;
+}
+
+.tw {
+  background-color:  #d3615e;
+  padding: 5px 9px;
+}
+
+.ig {
+  background-color:  #d3615e;
+  padding: 5px 11px;
+}
+button {
+    background-color: #2e3539;
+    color: white;
+    padding: 3px 14px;
+    border: none;
+    border-radius: 3px;
+}
+
 </style>
